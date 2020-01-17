929f057720fb70756d203ff9399388c9ba47e076
jbswizz75 <jb.veesler@gmail.com>
2020-01-17 14:40:52 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mds3_integrationtesting_2020
+
+# Launch API
+
+node index.js
+
+# Launch test
+
+npm test 
--- test/test.js
+++ test/test.js
@@ -20,7 +20,7 @@ const payloadColor = () => {
   nextCursor++;
   return { 'color': nextColor }
 }
-const getCurrentCulor = () => {
+const getCurrentColor = () => {
   return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
 }
 
@@ -58,7 +58,7 @@ it('Should return 404 request', () => {
 it('Should add new color', () => {
   return chai.request(app)
     .post('/colors')
-    .send(payloadColor('blue', 'white', 'red'))
+    .send(payloadColor('white'))
     .then((value) => {
       //Check if it's status 201
       expect(value).to.have.status(201);
@@ -69,9 +69,24 @@ it('Should add new color', () => {
       //Check if the result is an array
       expect(value.body.results).to.be.an('array');
       //Check if the contents include the new value
-      expect(value.body.results).to.include(getCurrentCulor());
+      expect(value.body.results).to.include(getCurrentColor());
     })
 });
 
-
-
+//Test for the request of the new color list
+it('Should return new color list', () => {
+  return chai.request(app)
+    .get('/colors')
+    .then((value) => {
+      //Check if it's status 200
+      expect(value).to.have.status(200);
+      //Check if it's a json
+      expect(value).to.be.json;
+      //Check if the body is an object
+      expect(value.body).to.be.an('object');
+      //Check if the result is an array
+      expect(value.body.results).to.be.an('array');
+      //Check if the contents include the new value
+      expect(value.body.results).to.include(getCurrentColor())
+    })
+});
