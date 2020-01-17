425b14f03096e4940a9e1493634e1ea5c853f5f6
jbswizz75 <jb.veesler@gmail.com>
2020-01-17 14:04:19 +0100

    Update

--- test/test.js
+++ test/test.js
@@ -23,4 +23,25 @@ const payloadColor = () => {
 const getCurrentCulor = () => {
   return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
 }
+
 // <-- FIN
+
+
+//Test for All Colors
+
+it('Should return all colors', () => {
+  return chai.request(app)
+    .get('/colors')
+    .then((value) => {
+      //Check if it's status 200
+      expect(value).to.have.status(200);
+      //Check if it's a json
+      expect(value).to.be.json;
+      //Check if the body is an object
+      expect(value.body).to.be.an('object');
+      //Check if the result is an array
+      expect(value.body.results).to.be.an('array');
+      //Check if the contents is correct
+      expect(value.contents).to.be.true;
+    })
+});
